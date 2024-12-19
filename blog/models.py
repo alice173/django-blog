f1b0083f444d860aef84db400b748ad602217ffc
Alice Mahtani <alice.mahtani@gmail.com>
2024-12-19 14:49:37 +0000

    add blog view

--- blog/models.py
+++ blog/models.py
@@ -11,7 +11,32 @@ class Post(models.Model):
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
     content = models.TextField()
+    excerpt = models.TextField(blank=True)
     created_on = models.DateTimeField(auto_now_add=True)
+    post_updated_on = models.DateTimeField(auto_now=True)
+
+  
+
+    class Meta:
+        ordering = ["created_on"]
+
+    def __str__(self):
+        return f"{self.title} | written by {self.author}"
+
     
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='commenter')
+    body = models.TextField()
+    approved = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+
+    class Meta:
+         ordering = ["-created_on"]
+
+    def __str__(self):
+        return f"Comment by {self.author}: {self.body}"
 
    
