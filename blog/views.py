9750aa0994e5bdefe52616fa240942c22b3867ff
Alice Mahtani <alice.mahtani@gmail.com>
2024-12-20 09:58:39 +0000

    add post list

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Post
 
 
 
 # Create your views here.
-
+class PostList(generic.ListView):
+    model = Post
+    queryset = Post.objects.all()
+    template_name = "post_list.html"
 
