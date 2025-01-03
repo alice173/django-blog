fc321c13b7752ea20308ec7c83eda06c8a36317a
Alice Mahtani <alice.mahtani@gmail.com>
2025-01-03 10:37:08 +0000

    add post list html

--- blog/models.py
+++ blog/models.py
@@ -14,6 +14,7 @@ class Post(models.Model):
     excerpt = models.TextField(blank=True)
     created_on = models.DateTimeField(auto_now_add=True)
     post_updated_on = models.DateTimeField(auto_now=True)
+    status = models.IntegerField(choices=STATUS, default=0)
 
   
 
--- codestar/settings.py
+++ codestar/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&yj1n83lc0afm$+9t05tk&5f41@jqjrk6#+qxc&&logk4^&0^-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1', 'localhost'
 ]
