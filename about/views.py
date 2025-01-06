a3f8a0518ef18fd086274b0b3335318ea963104c
Alice Mahtani <alice.mahtani@gmail.com>
2025-01-06 15:59:14 +0000

    add edit and delete comments

--- about/views.py
+++ about/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 
 from .models import About
+from .forms import CollaborateForm 
+from django.contrib import messages
 
 
 # Create your views here.
@@ -8,10 +10,24 @@ def about_me(request):
     """
     Renders the About page
     """
-    about = About.objects.all().order_by('-updated_on').first()
+  
 
+    if request.method == "POST":
+        collaborate_form = CollaborateForm(data=request.POST)
+        if collaborate_form.is_valid():
+            collaborate_form.save()
+            collaborate_form = CollaborateForm()
+            messages.add_message(
+                request, messages.SUCCESS,
+                'Comment submitted and awaiting approval')
+
+    about = About.objects.all().order_by('-updated_on').first()
+    collaborate_form = CollaborateForm()
+    
     return render(
         request,
         "about/about.html",
-        {"about": about},
+        {"about": about,
+         "collaborate_form": collaborate_form},
+
     )
